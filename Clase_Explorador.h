19ac3ca22ea45a72ad185e83a2b42de80dd7b763
Frany-oss <francesco8bassino@gmail.com>
2019-11-08 09:38:51 -0500

    commit

---
+++ Clase_Explorador.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Tree.h"
+#include <iostream>
+#include <string>
+
+enum Tipo{Nombre, Extension, Tamano, fecha_creacion};
+
+class Explorador{
+    std::string direccion_arch;
+    Tipo tipo;
+public:
+    Explorador(std::string direccion_arch, Tipo tipo) 
+    : direccion_arch(direccion_arch), tipo(tipo){ }
+
+    std::string getDirec(){ return direccion_arch;}
+    Tipo getTipo(){ return tipo;}
+
+    void setDirec(std::string direccion_arch){
+        this->direccion_arch = direccion_arch;
+    }
+    void setTipo(Tipo tipo){
+        this->tipo = tipo;
+    }
+
+};
--- Tree.h
+++ Tree.h
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <functional>
 #include <stdlib.h>
 #include <time.h>
