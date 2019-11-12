3e5fade676dc4ae2bc3e028a1dea31d261c99b98
AzraelVT <37254945+AzraelVT@users.noreply.github.com>
2019-11-12 01:28:23 -0500

    Add files via upload

---
+++ BST_2.hpp
@@ -0,0 +1,264 @@
+#pragma once
+
+#include <functional>
+#include <vector>
+
+
+#ifndef __BST_2_HPP__
+#define __BST_2_HPP__
+
+
+template<typename T, typename Comparable = T>
+class Tree {
+	struct Node {
+		T     elem;
+		Node* left;
+		Node* right;
+
+		Node(T elem) : elem(elem), left(nullptr), right(nullptr) {}
+	};
+
+	Node* root;
+	Node* root2;
+	vector <T> V;
+	int   len;
+	
+
+	std::function<Comparable(T)>  key;
+
+	void destroy(Node* node) {
+		if (node != nullptr) {
+			destroy(node->left);
+			destroy(node->right);
+			delete node;
+		}
+	}
+	Node* add(Node* node, T elem) {
+		if (node == nullptr) {
+			node = new Node(elem);
+			++len;
+		}
+		else if (key(elem) > key(node->elem)) {
+			node->right = add(node->right, elem);
+		}
+		else {
+			node->left = add(node->left, elem);
+		}
+		return node;
+	}
+
+	void inorder(Node* node, std::function<void(T)> proc) {
+		if (node != nullptr) {
+			inorder(node->left, proc);
+			proc(node->elem);
+			inorder(node->right, proc);
+		}
+	}
+
+	T find(Node* node, Comparable val) {
+		if (node == nullptr) {
+			return 0;
+		}
+		if (val == key(node->elem)) {
+			return node->elem;
+		}
+		else if (val < key(node->elem)) {
+			return find(node->left, val);
+		}
+		else {
+			return find(node->right, val);
+		}
+	}
+
+	Node * findDad(Node* node, Comparable val) {
+		if (node == nullptr) {
+			return nullptr;
+		}
+		if (node == root && val == root->elem) {
+			return nullptr;
+		}
+		if (val == key(node->left->elem) || val == key(node->right->elem)) {
+			return node;
+		}
+		else if (val < key(node->elem)) {
+			return findDad(node->left, val);
+		}
+		else {
+			return findDad(node->right, val);
+		}
+	}
+
+	Node* findNode(Node* node, Comparable val) {
+		if (node == nullptr) {
+			return nullptr;
+		}
+		if (val == key(node->elem)) {
+			return node;
+		}
+		else if (val < key(node->elem)) {
+			return findNode(node->left, val);
+		}
+		else {
+			return findNode(node->right, val);
+		}
+	}
+
+
+public:
+	Tree(std::function<Comparable(T)>  key = [](T a) {return a; })
+		: key(key), root(nullptr), len(0) {}
+	~Tree() {
+		destroy(root);
+	}
+	void add(T elem) {
+		root = add(root, elem);
+	}
+	bool remove(Comparable val) {
+		Node* aux = root;
+		Node* parent;
+		bool left;
+		while (aux != nullptr) {
+			if (val == key(aux->elem)) {
+				break;
+			}
+			else {
+				parent = aux;
+				if (val < key(aux->elem)) {
+					left = true;
+					aux = aux->left;
+				}
+				else {
+					left = false;
+					aux = aux->right;
+				}
+			}
+		}
+		if (aux == nullptr) return false;
+		if (aux->left == nullptr) {
+			if (aux == root)  root = aux->right;
+			else if (left)    parent->left = aux->right;
+			else              parent->right = aux->right;
+			delete aux;
+		}
+		else {
+			Node* aux2 = aux->left;
+			while (aux2->right != nullptr) {
+				parent = aux2;
+				aux2 = aux2->right;
+			}
+			if (aux2 == aux->left) {
+				aux->left = aux2->left;
+			}
+			else {
+				parent->right = aux2->left;
+			}
+			aux->elem = aux2->elem;
+			delete aux2;
+		}
+		return true;
+	}
+
+	T find(Comparable val) {
+		return find(root, val);
+	}
+
+	Node * findNode(Comparable val) {
+		return findNode(root, val);
+	}
+
+	Node * findDad(Comparable val) {
+		return findDad(root, val);
+	}
+
+	/*list<T>* findAll(Comparable val) {
+	return nullptr; // TODO!!
+	}*/
+
+	void inorder(std::function<void(T)> proc) {
+		inorder(root, proc);
+	}
+	
+
+	vector <T> FiltradoMayor(T n)
+	{
+		
+		root2 = findNode(n);
+		Node * root3 = root2;
+		Node * papi = findDad(root2->elem);//right
+		Node * hijo = root2;
+
+
+		//V.push_back(root2->elem); Descomentar para mayor e igual
+
+		while (root2->right != nullptr)
+		{
+			root2 = root2->right;
+			root3 = root2;
+			V.push_back(root2->elem);
+			while (root3->left != nullptr)
+			{
+				root3 = root3->left;
+				V.push_back(root3->elem);
+			}
+		}
+
+		if (papi != nullptr)
+		{
+			if (papi->elem > hijo->elem)
+			{
+				V.push_back(papi->elem);
+				FiltradoMayor(papi->elem);
+			}
+			else if (hijo->elem < root->elem)
+			{
+				V.push_back(root->elem);
+				FiltradoMayor(root->elem);
+			}
+		}
+
+		return V;
+	}
+
+	vector <T> FiltradoMenor(T n)
+	{
+
+		root2 = findNode(n);
+		Node * root3 = root2;
+		Node * papi = findDad(root2->elem);//left
+		Node * hijo = root2;
+
+
+		//V.push_back(root2->elem); Descomentar para mayor e igual
+
+		while (root2->left != nullptr)
+		{
+			root2 = root2->left;
+			root3 = root2;
+			V.push_back(root2->elem);
+			while (root3->right != nullptr)
+			{
+				root3 = root3->right;
+				V.push_back(root3->elem);
+			}
+		}
+
+		if (papi != nullptr)
+		{
+			if (papi->elem < hijo->elem)
+			{
+				V.push_back(papi->elem);
+				FiltradoMenor(papi->elem);
+			}
+			else if (hijo->elem > root->elem)
+			{
+				V.push_back(root->elem);
+				FiltradoMenor(root->elem);
+			}
+
+		}
+
+		return V;
+	}
+};
+
+#endif
