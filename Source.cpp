d289fb8b08fdad35e3e598049ab32c5514f9dd09
Frany-oss <francesco8bassino@gmail.com>
2019-11-12 22:25:40 -0500

    commit

--- Source.cpp
+++ Source.cpp
@@ -40,14 +40,24 @@ int main(){
 	avlfec->add(new Archivo("Word2", "txt", 90.10, 1)	);
 	avlfec->add(new Archivo("Gaaa", "gaa", 64.44, 3)    );
 
+	avlnom->add(new Archivo("Word", "txt", 112.85, 4));
+	avlnom->add(new Archivo("Power", "ppt", 180.44, 5));
+	avlnom->add(new Archivo("Word2", "txt", 90.10, 1));
+	avlnom->add(new Archivo("Gaaa", "gaa", 64.44, 3) );
+
 
 	vector <Archivo*> V = avlfec->FiltradoMayor(1);
+	vector <Archivo*> V2 = avlnom->FiltradoMayor("Gaaa");
 
 	cout << V.size() << endl;
 
 	cout << "	     NOMBRE EXTEN  TAM  CREACION" << endl;
 	for (int i = 0; i < V.size(); i++){
 		cout << "Archivo " << i << ":"; Mostrar(V[i]);
+	} cout << endl;
+
+	for (int i = 0; i < V2.size(); i++){
+		cout << "Archivo " << i << ":"; Mostrar(V2[i]);
 	}
 
 	//Mostrar(avlnom->find("Gaaa"));
