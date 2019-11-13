cc24217dedca819931f792eebad234b90a1cd913
Roberto Vega <55059546+peruviantorch@users.noreply.github.com>
2019-11-13 00:49:54 -0500

    Add files via upload
    aqui se extraen los atributos de los archivos y carpetas

--- Archivo.hpp
+++ Archivo.hpp
@@ -3,34 +3,34 @@
 #include <string>
 
 using namespace std;
-//using namespace System;
+using namespace System;
 
 class Archivo
 {
 private:
 	string	nombre		;
 	string	extension	;
-	int	tamano		    ;
-	int		fecha		; // 20190402
+	int	tamaño		;
+	int		fecha		;
 public:
 
 	Archivo(string nom, string ext, int tam, int fec)
 	{
 		nombre = nom;
 		extension = ext;
-		tamano = tam;
+		tamaño = tam;
 		fecha = fec;
 	}
 
 
 	string	getnombre	 	() {return nombre		;}
 	string	getextension  	() {return extension	;}
-	int	    gettamano		() {return tamano		;}
+	int	gettamaño		() {return tamaño		;}
 	int		getfecha		() {return fecha		;}
 
 	void setnombre		(string	nombre		) { this->nombre		= nombre	;}	
-	void setextension	(string	extension	) { this->extension	= extension	    ;}
-	void settamano		(int	tamano		) { this->tamano		= tamano	;}	
-	void setfecha		(int	fecha		) { this->fecha		= fecha		    ;}
+	void setextension	(string	extension	) { this->extension	= extension	;}
+	void settamaño		(int	tamaño		) { this->tamaño		= tamaño	;}
+	void setfecha		(int	fecha		) { this->fecha		= fecha		;}
 
 };
--- Source.cpp
+++ Source.cpp
@@ -1,76 +1,117 @@
 #include <iostream>
-#include <vector>
+#include <fstream>
+#include <experimental/filesystem>
 #include <string>
+#include <stdlib.h>
+#include <chrono>
+#include <iomanip>
+#include <vector>
 #include "Archivo.hpp"
-#include "avl.hpp"
 
+using namespace std;
+namespace fs = std::experimental::filesystem;
+using namespace std::chrono_literals;
 
+int ConvertirMes(string fecha) {
+	int mesNum = 0;
+	if (fecha.substr(4, 3) == "Jan") mesNum = 01;
+	if (fecha.substr(4, 3) == "Feb") mesNum = 02;
+	if (fecha.substr(4, 3) == "Mar") mesNum = 03;
+	if (fecha.substr(4, 3) == "Apr") mesNum = 04;
+	if (fecha.substr(4, 3) == "May") mesNum = 05;
+	if (fecha.substr(4, 3) == "Jun") mesNum = 06;
+	if (fecha.substr(4, 3) == "Jul") mesNum = 07;
+	if (fecha.substr(4, 3) == "Aug") mesNum = 8;
+	if (fecha.substr(4, 3) == "Sep") mesNum = 9;
+	if (fecha.substr(4, 3) == "Oct") mesNum = 10;
+	if (fecha.substr(4, 3) == "Nov") mesNum = 11;
+	if (fecha.substr(4, 3) == "Dec") mesNum = 12;
 
+	else return mesNum;
+}
+
+string Imprimirfecha(string fecha) {
+
+	int mess = ConvertirMes(fecha);
+
+	if (mess == 8 || mess == 9) {
+		string Fecha = fecha.substr(8, 2) + '0' + to_string(mess) + fecha.substr(20, 4);
+		return Fecha;
+	}
+
+	else {
+	string Fecha = fecha.substr(8, 2) + to_string(mess) + fecha.substr(20, 4);
+	return Fecha;
+	}
+}
+
+string ImprimirExtension(string nombre) {
+
+	/*string ext = nombre.substr(nombre.length() - 4, 4);
+	return ext;*/
+
+	for (int i = 0; i < nombre.length(); i++) {
+		if (nombre[i] == '.') return nombre.substr(i+1, nombre.length() - i);
+	}
+}
 
 int main()
 {
-	
-	typedef AVLTree <Archivo*, string, nullptr> AVLNom;
-	typedef AVLTree <Archivo*, string, nullptr> AVLExt;
-	typedef AVLTree <Archivo*, int, nullptr> AVLTam;
-	typedef AVLTree <Archivo*, int, nullptr> AVLFec;
 
-	auto knom = [](Archivo * A) { return A->getnombre(); };
-	auto kext = [](Archivo * A) { return A->getextension(); };
-	auto ktam = [](Archivo * A) { return A->gettamaï¿½o(); };
-	auto kfec = [](Archivo * A) { return A->getfecha(); };
+	vector <string> nombre;
+	vector <string> extension;
+	vector <int> tamano;
+	vector <string> fecha;
 
-	AVLNom * avlnom = new AVLNom(knom);
-	AVLExt * avlext = new AVLExt(kext);
-	AVLTam * avltam = new AVLTam(ktam);
-	AVLFec * avlfec = new AVLFec(kfec);
+	string path = "D:\\UPC\\arqui\\semana 1";
+		for (auto entry : fs::directory_iterator(path)) {
+			//NOMBRE
+			cout << entry.path() << endl;
 
+			string name = entry.path().filename().string();
+			nombre.push_back(name);
+			
+			//TAMAñO DE ARCHIVO. SI ES UN FOLDER ES 0 bytes
+			try {
+				std::cout << "File size = " << fs::file_size(entry.path()) << " bytes" << '\n';
+			}
+			catch (fs::filesystem_error& e) {
+				std::cout << "folder = 0 bytes" << '\n';
+			}
+			int size = fs::file_size(entry.path());
+			tamano.push_back(size);
 
-	auto Mostrar = [](Archivo * A)
-	{
-		cout << " ( " << A->getnombre() << " , " << A->getextension() << " , " << A->gettamaï¿½o() << " , " << A->getfecha() << " )" << endl;
-	};
+			//FECHA DE ULTIMA MODIFICACION
+			auto ftime = fs::last_write_time(entry.path());
+			time_t cftime = decltype(ftime)::clock::to_time_t(ftime); // assuming system_clock
+			//cout << ConvertirMes(asctime(localtime(&cftime)));
+			//Imprimirfecha(asctime(localtime(&cftime)));
+			string date = Imprimirfecha(asctime(localtime(&cftime)));
+			cout << date << endl;
+			fecha.push_back(date);
 
-	//auto add = [&](Archivo * A)
-	//{
-	//	avlnom->add(A);
-	//	avlext->add(A);
-	//	avltam->add(A);
-	//	avlfec->add(A);
-	//};
+			//EXTENSION
 
+			string ext;
+				 ext = entry.path().filename().string();
+				cout << ImprimirExtension(ext) << endl;
+			extension.push_back(ImprimirExtension(ext));
+			cout << endl;
+		}
 
-	//avlfec->add(new Archivo("Gaaa", "gaa", 64.44, 9));
-	//avlfec->add(new Archivo("Word", "txt", 112.85, 4)	);
-	//avlfec->add(new Archivo("Power", "ppt", 180.44, 5)	);
-	//avlfec->add(new Archivo("Word2", "txt", 90.10, 1)	);
-	//avlfec->add(new Archivo("Gaaa", "gaa", 64.44, 3)  );
-	//avlfec->add(new Archivo("Gaaa", "gaa", 64.44, 7));
-
-	//string aux ("Word ");
-	//string aux2("Power");
-	//string aux3("Word2");
-	//string aux4("Gaaa ");
-//
-	//string a = aux.substr ( aux.length() - 1, 1);
-	//string b = aux2.substr(aux2.length() - 1, 1);
-	//string c = aux3.substr(aux3.length() - 1, 1);
-	//string d = aux4.substr(aux4.length() - 1, 1);
-
-
-
-	vector <Archivo*> V = avlfec->antiinorderV();
-
-	//avlfec->inorder(Mostrar);
-
-	for (int i = 0; i < V.size(); i++)
-	{
-		Mostrar(V[i]);
-	}
+		//prueba de que se guardan los atributos
+		for (int i; i < nombre.size(); i++) {
+			cout << nombre[i] << endl;
+			cout << extension[i] << endl;
+			cout << tamano[i] << endl;
+			cout << fecha[i] << endl;
+		}
+		vector <Archivo> files;
+		for (int i; i < nombre.size(); i++) {
+		files.push_back(Archivo(nombre[i], extension[i], tamano[i], stoi(fecha[i])));
+		}
 
 
-
-	//Mostrar(avlnom->find("Gaaa"));
 
 	system("pause");
 	return 0;
