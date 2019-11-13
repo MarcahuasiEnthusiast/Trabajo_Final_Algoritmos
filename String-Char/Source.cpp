07276fcd4f3aa0e79a76305380937aff77f515e1
Frany-oss <francesco8bassino@gmail.com>
2019-11-12 21:11:08 -0500

    commit

--- String-Char/Source.cpp
+++ String-Char/Source.cpp
@@ -1,28 +1,39 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include "Tree.hpp"
 using namespace std;
-
 //#include "BSTArchivos.hpp"
 int main(){
+	
+	Tree <string> T;
+	vector <string> V;
 
-	string str;
-	string str_2;
+	string aux("wewggn");
+	string aux2("nmvnk");
+	string aux3("tuqe");
+	string aux4("wa");
+	string aux5("dnuyu");
+	string aux6("wrwz");
 
-	cout << "Primer string: ";
-	cin >> str;
-	cout << "Segundo string: ";
-	cin >> str_2;
+	string a = aux.substr(aux.length()   - 1, 1);
+	string b = aux2.substr(aux2.length() - 1, 1);
+	string c = aux3.substr(aux3.length() - 1, 1);
+	string d = aux4.substr(aux4.length() - 1, 1);
+	string e = aux5.substr(aux5.length() - 1, 1);
+	string f = aux6.substr(aux6.length() - 1, 1);
 
-	string aux = str.substr(str.length() - 1, 1);
-	string aux_2 = str_2.substr(str_2.length() - 1, 1);
+	T.add(a);
+	T.add(f);
+	T.add(b);
+	T.add(c);
+	T.add(d);
+	T.add(e);
+	
+	V = T.FiltrarMenor(f);
 
-
-	if(aux_2 < aux){
-		cout << "SI";
-	} else{
-		cout << "NO";
+	for (int i = 0; i < V.size(); i++)
+	{
+		cout << V[i] << endl;
 	}
 
 	return 0;
